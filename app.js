b37c0fe6c2e498b3041502f8387b1b35428772b0
Sofia Dailoff <s.sdailoff@gmail.com>
2022-10-29 14:36:47 -0300

    redirecciones

--- app.js
+++ app.js
@@ -14,39 +14,39 @@ iconoMenu.addEventListener('click', (e)=>{
     }
 
     
-})
+});
 */
 
-const nombre = document.getElementById("name")
-const email = document.getElementById("email")
-const mensaje = document.getElementById("mensaje")
-const form = document.getElementById("form")
-const parrafo = document.getElementById("warnings")
+const nombre = document.getElementById("name");
+const email = document.getElementById("email");
+const mensaje = document.getElementById("mensaje");
+const form = document.getElementById("form");
+const parrafo = document.getElementById("warnings");
 
 form.addEventListener("submit", e=>{
-    e.preventDefault()
-    let warnings = ""
-    entrar = false
-    let regex_email = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+    e.preventDefault();
+    let warnings = "";
+    entrar = false;
+    let regex_email = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
 
     if(nombre.value.length <3){
-        warnings += `Nombre no valido<br>`
-        entrar = true
+        warnings += `Nombre no valido<br>`;
+        entrar = true;
     }
     if(mensaje.value.length <10){
-        warnings += `Mensaje no valido <br>`
-        entrar = true
+        warnings += `Mensaje no valido <br>`;
+        entrar = true;
     }
     
     if(!regex_email.test(email.value)){
-        warnings += `Email no valido <br>`
-        entrar = true
+        warnings += `Email no valido <br>`;
+        entrar = true;
     }
     if (entrar){
-        parrafo.innerHTML = warnings
+        parrafo.innerHTML = warnings;
     }else{
-        parrafo.innerHTML = "Enviado"
+        parrafo.innerHTML = "Enviado";
     }
     
-})
+});
 
